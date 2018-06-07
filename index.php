f7437f09be295716bdd734bad9bfda7c14585f2e
Unknown <winand.geraldine@hotmail.com>
2018-06-07 13:32:02 +0200

    structure

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+		<link rel="stylesheet" type="text/css" href="css/index.css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+		<title>Collectif Corail</title>
+</head>
+<body>
+	<section>
+		<main class="main__container">
+
+			<div class="main__slide" id="lobby">
+				
+			</div>
+
+			<div class="main__slide" id="about_us">
+				
+			</div>
+
+			<div class="main__slide" id="agenda">
+				
+			</div>
+
+			<div class="main__slide" id="gallery">
+				
+			</div>
+
+			<div class="main__slide" id="contact">
+				
+			</div>
+			
+		</main>
+
+
+
+		<footer class="footer__corail">
+			<ul>
+				<li><i class="fas fa-map-marker"> Rue Vanderkindere 293 1180 - Bruxelles </i></li>		
+				<li><i class="fas fa-mobile-alt"> 0485 71 73 03 </i></li>
+				<li><i class="fab fa-facebook-square "></i></li>
+				<li><i class="fab fa-facebook-messenger"></i></li>
+				<li><i class="fab fa-youtube"></i></li>
+
+				
+			</ul>
+
+		</footer>
+
+                      
+			
+		
+	</section>
+
+</body>
+</html>
