b83309315802fb10cb103bfdcc8f6b8270d535d3
Mignot Morgan <MignotMorgan@Gmail.com>
2018-06-07 16:33:02 +0200

    calendrier

--- index.php
+++ index.php
@@ -29,7 +29,77 @@
 			</div>
 
 			<div class="main__slide" id="agenda">
-				
+			<h2>Calendrier</h2>
+			<div class="agenda">
+				<div class="table-responsive">
+					<table class="table table-condensed table-bordered">
+						<thead>
+							<tr>
+								<th>Date</th>
+								<th>Time</th>
+								<th>Event</th>
+							</tr>
+						</thead>
+						<tbody>
+							<!-- Single event in a single day -->
+							<tr>
+								<td class="agenda-date" class="active" rowspan="1">
+									<div class="dayofmonth">26</div>
+									<div class="dayofweek">Saturday</div>
+									<div class="shortdate text-muted">July, 2014</div>
+								</td>
+								<td class="agenda-time">
+									5:30 AM
+								</td>
+								<td class="agenda-events">
+									<div class="agenda-event">
+										<i class="glyphicon glyphicon-repeat text-muted" title="Repeating event"></i> 
+										Fishing
+									</div>
+								</td>
+							</tr>
+							
+							<!-- Multiple events in a single day (note the rowspan) -->
+							<tr>
+								<td class="agenda-date" class="active" rowspan="3">
+									<div class="dayofmonth">24</div>
+									<div class="dayofweek">Thursday</div>
+									<div class="shortdate text-muted">July, 2014</div>
+								</td>
+								<td class="agenda-time">
+									8:00 - 9:00 AM 
+								</td>
+								<td class="agenda-events">
+									<div class="agenda-event">
+										Doctor's Appointment
+									</div>
+								</td>
+							</tr>
+							<tr>
+								<td class="agenda-time">
+									10:15 AM - 12:00 PM 
+								</td>
+								<td class="agenda-events">
+									<div class="agenda-event">
+										Meeting with executives
+									</div>
+								</td>
+							</tr>
+							<tr>
+								<td class="agenda-time">
+									7:00 - 9:00 PM
+								</td>
+								<td class="agenda-events">
+									<div class="agenda-event">
+										Aria's dance recital
+									</div>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+						
 			</div>
 
 			<div class="main__slide" id="gallery">
@@ -57,6 +127,7 @@
 				<li><i class="fab fa-facebook-messenger"></i></li>
 				<li><i class="fab fa-youtube"></i></li>
 
+				<li><div class="button__slide" onclick="clickSlide()">Next</div></li>
 				
 			</ul>
 
--- main.js
+++ main.js
@@ -4,7 +4,16 @@ let wheelMain = (e) => {
 	// alert("scroll" + delta);
 	/*window.scrollTo(delta, 0);*/
 	document.documentElement.scrollLeft += delta;
-	console.log(document.documentElement.scrollLeft);
+	// console.log(document.documentElement.scrollLeft);
+}
+let screenWidth = document.documentElement.clientWidth;
+let clickSlide = (e) => {
+	let scrollLeft = Math.ceil(document.documentElement.scrollLeft/screenWidth)*screenWidth;
+
+	if(document.documentElement.scrollLeft == scrollLeft)
+		document.documentElement.scrollLeft += screenWidth;
+	else
+		document.documentElement.scrollLeft = scrollLeft;
 }
 
 window.onload=()=>{
