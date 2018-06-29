4abe5efdf89352235e6e477659c8397360a90391
Mignot Morgan <MignotMorgan@Gmail.com>
2018-06-29 13:59:55 +0200

    Ajout d'animation. Modification du scrolling

--- js/main.js
+++ js/main.js
@@ -1,23 +1,49 @@
+const slides = document.querySelectorAll(".main__slide");
 
-
-/**fonction de la roulette de la souris */
+/**fonction de la roulette de la souris pour le scrolling horizontale */
 let wheelMain = (e) => {
 	e.preventDefault();
-	let delta=e.deltaY
-	// alert("scroll" + delta);
-	/*window.scrollTo(delta, 0);*/
-	document.documentElement.scrollLeft += delta;
-	// console.log(document.documentElement.scrollLeft);
-}
-/**fonction du défilement horizontale du bouton inclus dans le footer. */
-let screenWidth = document.documentElement.clientWidth;
-let clickSlide = (e) => {
-	let scrollLeft = Math.ceil(document.documentElement.scrollLeft/screenWidth)*screenWidth;
-
-	if(document.documentElement.scrollLeft == scrollLeft)
-		document.documentElement.scrollLeft += screenWidth;
+	let screenWidth = document.documentElement.clientWidth;
+	let delta=screenWidth*0.2;//e.deltaY
+	if(e.deltaY < 0)
+		document.documentElement.scrollLeft -= delta;
 	else
-		document.documentElement.scrollLeft = scrollLeft;
+		document.documentElement.scrollLeft += delta;
+}
+/**fonction du défilement horizontale */
+let scrollSlide = () => {
+	let screenWidth = document.documentElement.clientWidth;
+	let currentSlide = Math.ceil(document.documentElement.scrollLeft/screenWidth);
+	if(currentSlide >= slides.length-1){
+		document.documentElement.scrollLeft = 0;
+	}
+	else{
+		let scrollLeft = currentSlide*screenWidth;
+		if(document.documentElement.scrollLeft == scrollLeft)
+			document.documentElement.scrollLeft += screenWidth;
+		else
+			document.documentElement.scrollLeft = scrollLeft;
+	}
+
+}
+/** click du bouton inclus dans le footer. */
+let clickSlide = (e) => {
+	scrollSlide();
+}
+/** click du bouton rond */
+const btn_circle = document.querySelector("#btn__img__arrow_circle");
+const clickCircle = (e) => {
+	/** supprime puis ajoute la class : "btn__img__arrow_circle_animation"  */
+	btn_circle.classList.remove("btn__img__arrow_circle_animation");
+	btn_circle.classList.add("btn__img__arrow_circle_animation");
+	/**lance l'animation */
+	btn_circle.style.webkitAnimationPlayState = 'running';
+	/**stop l'animation après 2 secondes */
+	let timer = setTimeout( () => {
+		btn_circle.style.webkitAnimationPlayState = 'paused';
+	}, 2000);
+	scrollSlide();
+
 }
 /**évènement du chargement de la page. */
 window.onload=()=>{
